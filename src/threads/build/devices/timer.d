805c8d4d9fa8a4d879a2db125c562f4685d49484
Kangmin Yoo <kaniblurous@gmail.com>
2011-05-24 00:16:12 +0900

    pr.c, swap.c added

--- src/threads/build/devices/timer.d
+++ src/threads/build/devices/timer.d
@@ -3,4 +3,5 @@ devices/timer.o: ../../devices/timer.c ../../devices/timer.h \
  ../../lib/inttypes.h ../../lib/stdio.h ../../lib/stdarg.h \
  ../../lib/stdbool.h ../../lib/stddef.h ../../lib/kernel/stdio.h \
  ../../threads/interrupt.h ../../threads/io.h ../../threads/synch.h \
- ../../lib/kernel/list.h ../../threads/thread.h
+ ../../lib/kernel/list.h ../../threads/thread.h ../../userprog/lru.h \
+ ../../lib/stdlib.h
