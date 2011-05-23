c63901955e02827ca59a2d74e9589b0a7a07f482
Kangmin Yoo <kaniblurous@gmail.com>
2011-05-24 00:16:46 +0900

    pr.c, swap.c(empty) added

---
+++ src/vm/build/vm/swap.d
@@ -0,0 +1,5 @@
+vm/swap.o: ../../vm/swap.c ../../vm/swap.h ../../lib/stdlib.h \
+ ../../lib/stddef.h ../../lib/stdbool.h ../../devices/disk.h \
+ ../../lib/inttypes.h ../../lib/stdint.h ../../threads/pte.h \
+ ../../threads/vaddr.h ../../lib/debug.h ../../threads/loader.h \
+ ../../userprog/pagedir.h
