93ded02655b40605de88fe6303e2e890d19913a7
kaniblurous <kaniblurous@gmail.com>
2011-05-24 07:35:27 +0000

    swap.h upload

--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,6 +1,16 @@
-#include <stdlib.h>
-#include <stdbool.h>
-#include "devices/disk.h"
+#include <list.h>
 
-bool swap_in(uint32_t *,uint32_t *);
-bool swap_out(uint32_t *);
+struct swap_slot
+{
+	uint32_t number;
+	uint32_t index;
+	struct list_elem elem;
+};
+
+struct list swap_table;
+
+void swap_init();
+int has_empty_slot(uint32_t swap_slot_index);
+void swap_in(void *page, uint32_t swap_slot_index);
+void swap_out(void *page, uint32_t swap_slot_index);
+
